42839e661458c607da405614d151c7afdadc42d7
v.alekseev <v.alekseev@luntry.ru>
2025-01-21 03:41:57 +0300

    update /images/runtime rq to post

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,12 +5,12 @@ import logging
 
 
 class Const:
-    BASE_URL = "https://viz.aqa.luntry.com/api/v1"
-    API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI5NjgzZTkzLTAxZDEtNDYwOS1iYTg2LWY3ZDNmMGFhYzdkOSIsImlhdCI6MTczMTYxNzc3MywiZXhwIjoxNzM5MzkzNzczfQ.CRnB06FIJo_evMqYvpLV2nzbRb5l-l8-jCxOs5wL24c"
-    CLUSTER_ID = "4aa5b484-df96-4f5b-b9de-7ee6fdc9f002"
+    BASE_URL = "http://localhost:8080/api/v1"
+    API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI4NDY5MWVjLWJlYzYtNDc5My05ZDFhLTNmODM4ZmExMWNhMiIsImlhdCI6MTczNzAyNDA3MCwiZXhwIjoxNzQ0ODAwMDcwfQ.8GKXnxvEaoLgufHkQyygIjhQxWmwU1xljgbBY3O5qeE"
+    CLUSTER_ID = "77233cc9-cbc0-4ae6-b466-69d1c268901d"
     APP_NAME = "registry.luntry.com/luntry/vuln-op"
     FILE_SBOM_EXAMPLE = "sbom_report.json"
-    DIR_TEST_DATA = "/home/ech20/test/job_luntry/test_data"
+    DIR_TEST_DATA = "/home/vsevolod/2code/job_luntry/test_data"
 
 
 class CommonVars:
@@ -46,25 +46,37 @@ class Tools:
 class Tests:
     @pytest.mark.dependency(name="get_image_digest")
     def test_get_image_digest(self):
+
         # pre
         url = f"{Const.BASE_URL}/images/runtime"
 
         rq_params = {
             "clusterId": Const.CLUSTER_ID,
-            "applicationName": Const.APP_NAME,
-            "usedImages": "true",
         }
 
-        rq_headers = {"x-access-token": Const.API_TOKEN}
+        rq_payload = json.dumps({
+            "namespaces": [
+                "e4ea430c-c4d7-4180-bb2d-901b1a8f9b4e"
+            ],
+            "usedImages": True
+        })
+
+        rq_headers = {
+            "x-access-token": Const.API_TOKEN,
+            'Content-Type': 'application/json',
+        }
 
         # do
-        response = requests.get(url, verify=False, params=rq_params, headers=rq_headers)
+        response = requests.request("POST", url, headers=rq_headers, params=rq_params, data=rq_payload)
         response.raise_for_status()
+        print(response.text)
+
 
         # process
         json_response = response.json()
         CommonVars.image_digest = json_response[0]["digest"]
         logging.info(f"image_digest: {CommonVars.image_digest}")
+
 
         # check
         assert response.status_code == 200
